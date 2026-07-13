7c45e8b55ed427d215440b5286db497c9b5c10f4
vladislav900087 <fyn5yhsj4h@privaterelay.appleid.com>
2026-07-13 23:20:02 +0500

    fix: database and redis url correct

--- app/core/redis.py
+++ app/core/redis.py
@@ -1,6 +1,6 @@
 import redis
 
-redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
+redis_client = redis.Redis(host='redis://red-d9ai54ok1i2s73c5pev0:6379', port=6379, decode_responses=True)
 
 EXPIRATION_TIME = 300
 
