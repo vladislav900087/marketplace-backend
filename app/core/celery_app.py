0270c5263e30dad4e2e7223c5802ddfec410289b
vladislav900087 <fyn5yhsj4h@privaterelay.appleid.com>
2026-07-14 16:28:07 +0500

    chore: Adding origins to the Middleware CORS

--- app/core/celery_app.py
+++ app/core/celery_app.py
@@ -1,9 +1,18 @@
+import os
 from celery import Celery
 
+redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
+celery_app = Celery('marketplace', broker=redis_url, backend=redis_url)
 
-celery_app = Celery('marketplace', broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')
+# ssl parameters enabled
+if redis_url.startswith('rediss://'):
+    celery_app.conf.update(
+        broker_use_ssl={"ssl_cert_reqs": "none"},
+        redis_backend_use_ssl={"ssl_cert_reqs": "none"}
+    )
 
 import app.tasks.email_tasks
 import app.tasks.result_tasks
 celery_app.autodiscover_tasks(['app.tasks'])
+print('ACTIVE REDIS URL:', redis_url)
 print(celery_app.tasks.keys())
--- app/main.py
+++ app/main.py
@@ -17,8 +17,14 @@ from app.api.profile_api import profile_router
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    'https://marketplace-frontend-vladislav7.vercel.app'
+]
 # add middleware
-app.add_middleware(CORSMiddleware, allow_origins=['http://localhost:5173'], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 # add routers
 app.include_router(auth_router, tags=['Authentication'])
