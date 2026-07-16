7c56696c3977dce33d6129e395f1fe0449f78691
vladislav900087 <fyn5yhsj4h@privaterelay.appleid.com>
2026-07-16 13:26:06 +0500

    Fixed fetching redis url

--- app/core/celery_app.py
+++ app/core/celery_app.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-redis_url = os.getenv('EXTERNAL_REDIS_URL')
+redis_url = os.getenv('EXTERNAL_REDIS_URL', 'redis://localhost:6379')
 celery_app = Celery('marketplace', broker=redis_url, backend=redis_url)
 
 # ssl parameters enabled
