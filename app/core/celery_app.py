f4b9aaeaad28c794f72f1e587497735d9acfb0a7
vladislav900087 <fyn5yhsj4h@privaterelay.appleid.com>
2026-07-17 13:36:44 +0500

    Added celery env urls selection logic

--- app/core/celery_app.py
+++ app/core/celery_app.py
@@ -1,21 +1,23 @@
 import os
 from celery import Celery
 
-redis_url = os.getenv('REDIS_URL')
-if not redis_url:
-    raise RuntimeError('REDIS_URL is not configured')
-celery_app = Celery('marketplace', broker=redis_url, backend=redis_url)
+broker_url = os.getenv('CELERY_BROKER_URL') or os.getenv('REDIS_URL')
+result_backend = os.getenv('CELERY_RESULT_BACKEND') or broker_url
+
+if not broker_url:
+    raise RuntimeError('CELERY_BROKER_URL OR REDIS_URL is not configured')
+celery_app = Celery('marketplace', broker=broker_url, backend=result_backend)
 
 # ssl parameters enabled
-if redis_url is not None:
-    if redis_url.startswith('rediss://'):
-        celery_app.conf.update(
-            broker_use_ssl={"ssl_cert_reqs": "none"},
-            redis_backend_use_ssl={"ssl_cert_reqs": "none"}
-        )
+
+if broker_url.startswith('rediss://'):
+    celery_app.conf.update(
+        broker_use_ssl={"ssl_cert_reqs": "none"},
+        redis_backend_use_ssl={"ssl_cert_reqs": "none"}
+    )
 
 import app.tasks.email_tasks
 import app.tasks.result_tasks
 celery_app.autodiscover_tasks(['app.tasks'])
-print('ACTIVE REDIS URL:', redis_url)
+print('ACTIVE REDIS URL:', broker_url)
 print(celery_app.tasks.keys())
